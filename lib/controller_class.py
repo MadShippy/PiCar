044d0c772f6cb7ed6cc80a624524872214913458
MadShippy <97624159+MadShippy@users.noreply.github.com>
2022-01-28 22:32:44 -0800

    Update controller_class.py

--- lib/controller_class.py
+++ lib/controller_class.py
@@ -1,1 +1,14 @@
+import sys
+sys.path.append(r'/home/kaanb/RobotSystems/lib')
+import time
+import numpy
+from picarx_improved import Picarx
 
+class Controller(Interpreter):
+    def __init__(self, scalingFactor=20):
+        super().__init__()
+        self.scalingFactor = scalingFactor
+
+    def steering(self):
+        direction, degree = self.edge_detection()
+        steer = int(self.scalingFactor * degree)
