66bd6b69050295cfccd8b5f003d351f699e19baf
MadShippy <97624159+MadShippy@users.noreply.github.com>
2022-01-28 22:29:25 -0800

    Update sensor_class.py

--- lib/sensor_class.py
+++ lib/sensor_class.py
@@ -1,1 +1,19 @@
+import sys
+sys.path.append(r'/home/kaanb/RobotSystems/lib')
+import time
+import numpy
+from picarx_improved import Picarx
 
+class Sensor(Picarx):
+    def __init__(self):
+        super().__init__()
+        self.C0 = ADC("A0")
+        self.C1 = ADC("A1")
+        self.C2 = ADC("A2")
+        # ADC values for grayscale module
+    def get_adc_value(self):
+        adc_value_list = []
+        adc_value_list.append(self.C0.read())
+        adc_value_list.append(self.C1.read())
+        adc_value_list.append(self.C2.read())
+        return adc_value_list
