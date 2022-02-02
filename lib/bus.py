e813a6b0d68ee5997ddde7ed6598edd0c22369d0
MadShippy <97624159+MadShippy@users.noreply.github.com>
2022-02-02 12:58:03 -0800

    Create bus.py

---
+++ lib/bus.py
@@ -0,0 +1,19 @@
+from readerwriterlock import rwlock
+
+
+class Bus:
+    def __init__(self):
+        self.message = None
+        self.lock = rwlock.RWLockWriteD()
+
+    def read(self):
+        with self.lock.gen_rlock():
+            msg = self.message
+        return msg
+
+    def write(self, msg):
+        with self.lock.gen_wlock():
+            self.message = msg
+
+
+if __name__ == '__main__':
