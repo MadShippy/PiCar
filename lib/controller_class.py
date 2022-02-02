b03b2d4cbf64a831280276e03a73d7fb3df4b047
MadShippy <97624159+MadShippy@users.noreply.github.com>
2022-02-02 13:11:36 -0800

    Update controller_class.py

--- lib/controller_class.py
+++ lib/controller_class.py
@@ -6,9 +6,20 @@ from picarx_improved import Picarx
 
 class Controller(Interpreter):
     def __init__(self, scalingFactor=20):
-        super().__init__()
         self.scalingFactor = scalingFactor
 
-    def steering(self):
+    def steering(self, motor):
         direction, degree = self.edge_detection()
         steer = int(self.scalingFactor * degree)
+        motor.set_dir_servo_angle(angle)
+        motor.forward(10)
+
+
+def consumer_producer(control_bus, delay_s):
+    control = Controller()
+    motor = motor_commands.MotorCommands()
+    while True:
+        get_val = control_bus.read()
+        angle = control.main_control(get_val, motor)
+        control_bus.write(angle)
+        time.sleep(delay_s)
